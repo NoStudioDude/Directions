769fda4c85701ff7665cea83e7de146376cd498d
Rui Nunes <NoStudioDude@hotmail.com>
2015-06-15 23:35:26 +0200

    initial commit
    .

---
+++ Directions/Assets/Scripts/Arrow/ArrowScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum eArrow { Left, Right, Up, Down }
+public class ArrowScript : MonoBehaviour {
+
+    public eArrow arrowType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Directions/Assets/Scripts/Game/BoardManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardManager : MonoBehaviour {
+
+    [SerializeField]
+    public class BoardSize
+    {
+        public int Maximun;
+        public int Minimun;
+
+        public BoardSize(int min, int max)
+        {
+            Minimun = min;
+            Maximun = max;
+        }
+    }
+
+    GameManager gManager;
+    
+	void Start () {
+        gManager = GameManager.Instance;
+	}
+	
+	void Update () {
+	
+	}
+}
